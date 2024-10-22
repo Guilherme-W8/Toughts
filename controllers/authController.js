e97390fcbc2a5610e7db43670c72e5620c9970e4
guilherme-w8 <gtavares739@gmail.com>
2024-10-22 16:52:14 -0300

    register post (verificar senhas)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,6 @@
+import User from '../models/User.js';
+import bcrypt from 'bcryptjs';
+
 export default class AuthController {
     static login(request, response){
         response.render('auth/login');
@@ -5,5 +8,17 @@ export default class AuthController {
 
     static register(request, response){
         response.render('auth/register');
+    }
+
+    static async registerPost(request, response){
+        const {name, email, password, confirmpassword} = request.body;
+
+        // Verificar se as senhas correspondem
+        if(password != confirmpassword){
+            // flash message
+            request.flash('message', 'As senhas precisam ser iguais');
+            response.render('auth/register');
+            return;
+        }
     }
 }
