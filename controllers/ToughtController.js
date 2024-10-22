14fb8556b1e55fba34733e268a43556424ef28d7
Guilherme-W8 <gtavares739@gmail.com>
2024-10-21 22:27:24 -0300

    async func

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,8 +2,7 @@ import Tought from '../models/Tought.js';
 import User from '../models/User.js';
 
 export default class ToughtController {
-    static showToughts(request, response){
-        console.log('estou aqui')
+    static async showToughts(request, response){
         response.render('toughts/home');
     }
 }
