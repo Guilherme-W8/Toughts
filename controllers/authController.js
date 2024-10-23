11a454a32744b11c971d7cdbcb2d733b7eda3118
guilherme-w8 <gtavares739@gmail.com>
2024-10-23 13:14:22 -0300

    validate and create an user

--- controllers/authController.js
+++ controllers/authController.js
@@ -15,10 +15,49 @@ export default class AuthController {
 
         // Verificar se as senhas correspondem
         if(password != confirmpassword){
-            // flash message
+            // flash message password
             request.flash('message', 'As senhas precisam ser iguais');
             response.render('auth/register');
             return;
+        }
+
+        // Checando se User já existe
+        const checkUserExists = await User.findOne({
+            where: {
+                email: email
+            }
+        });
+
+        if(checkUserExists){
+            // flash message email exists
+            request.flash('message', 'Email já cadastrado');
+            response.render('auth/register');
+            return;
+        }
+
+        // Criando senha
+        const salt = bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password, salt); 
+
+        const user = {
+            name: name,
+            email: email,
+            password: hashedPassword
+        }
+
+        try {
+            const createdUser = await User.create(user);
+
+            // Inicializando sessao
+            request.session.userId = createdUser.id;
+
+            request.flash('message', 'Cadastro realizado com sucesso');
+
+            request.session.save(() => {
+                response.redirect('/');
+            });
+        } catch (error){
+            console.log(error);
         }
     }
 }
