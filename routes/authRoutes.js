3a6b725294a9b227e0b5c02860d1dea2c924f2f4
guilherme-w8 <gtavares739@gmail.com>
2024-10-23 15:09:49 -0300

    organize routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,9 +3,13 @@ import AuthController from '../controllers/authController.js';
 
 const router = express.Router();
 
-router.get('/login', AuthController.login);
+// Register routes
 router.get('/register', AuthController.register);
 router.post('/register', AuthController.registerPost);
+
+// Login routes
+router.get('/login', AuthController.login);
+router.post('/login', AuthController.loginPost);
 router.get('/logout', AuthController.logout);
 
 export default router;
