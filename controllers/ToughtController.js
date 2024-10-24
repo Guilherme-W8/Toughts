9930e4fdd4d368a6cf07124c1b35953ff602e96d
guilherme-w8 <gtavares739@gmail.com>
2024-10-24 14:05:35 -0300

    dashboard function controller

--- controllers/ToughtController.js
+++ controllers/ToughtController.js
@@ -2,7 +2,11 @@ import Tought from '../models/Tought.js';
 import User from '../models/User.js';
 
 export default class ToughtController {
-    static async showToughts(request, response){
+    static async showToughts(request, response) {
         response.render('toughts/home');
+    }
+
+    static async dashboard(request, response) {
+        response.render('toughts/dashboard');
     }
 }
